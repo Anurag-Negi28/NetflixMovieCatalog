d7f908de173fded84c7176381e896e28c372a0e2
Anurag-Negi28 <an6265081@gmail.com>
2024-09-19 19:32:56 +0000

    Fixing CI/CD

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi Dhruv Bhadwe! This app is an API, there is no UI ;-)"
+    return "Hi Dhruv Laude! This app is an API, there is no UI ;-)"
 
 
 @app.route('/discover')
